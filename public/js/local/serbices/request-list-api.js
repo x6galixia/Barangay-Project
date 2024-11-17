82b48ad234a1df75d9c9a932bb9cec8229af2513
Eugene Gerna <eugenegerna02@gmail.com>
2024-11-17 11:15:37 -0800

    certificate display oks

--- public/js/local/serbices/request-list-api.js
+++ public/js/local/serbices/request-list-api.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", async function () {
                 <td>${request.fname} ${request.mname ? request.mname : ''} ${request.lname}</td>
                 <td>${request.purpose}</td>
                 <td>${formattedDate}</td>
-                <td><button id="${request.purpose}" onclick="popUp_three_dot(this)">Print</button></td>
+                <td><button data-purpose="${request.purpose}" onclick="processCertificate(this)">Process</button></td>
             `;
         requestTableBody.appendChild(row);
       });
@@ -62,3 +62,125 @@ document.addEventListener("DOMContentLoaded", async function () {
   }
   // attachDotEventListeners();
 });
+
+window.processCertificate = function (button) {
+  const purpose = button.getAttribute('data-purpose');
+  document.querySelector(".overlay").classList.toggle("visible");
+  document.querySelector("#process-certificate").classList.toggle("visible");
+  document.getElementById("certificateHeading").innerText = purpose;
+
+  const viewCertificate = document.getElementById("viewCertificate");
+
+  if (purpose === 'Brgy. Clearance') {
+    viewCertificate.addEventListener('click', function () {
+
+    })
+  }
+  else if (purpose === 'Building Clearance') {
+  }
+  else if (purpose === 'Burial Certificate') {
+  }
+  else if (purpose === 'Business Clearance') {
+  }
+  else if (purpose === 'Business Closure') {
+  }
+  else if (purpose === 'Good Moral') {
+  }
+  else if (purpose === 'Guardianship') {
+    viewCertificate.addEventListener('click', function () {
+      document.getElementById("certificate").classList.toggle("visible");
+      document.getElementById("guardianship").classList.toggle("visible");
+
+      document.querySelector(".closeCertificate").addEventListener("click", function () {
+        document.getElementById("certificate").classList.remove("visible");
+        document.getElementById("guardianship").classList.remove("visible");
+      })
+    })
+  }
+  else if (purpose === 'Income') {
+    viewCertificate.addEventListener('click', function () {
+      document.getElementById("certificate").classList.toggle("visible");
+      document.getElementById("income").classList.toggle("visible");
+
+      document.querySelector(".closeCertificate").addEventListener("click", function () {
+        console.log("close click");
+        document.getElementById("certificate").classList.remove("visible");
+        document.getElementById("income").classList.remove("visible");
+      })
+    })
+  }
+  else if (purpose === 'Indigency') {
+    viewCertificate.addEventListener('click', function () {
+      document.getElementById("certificate").classList.toggle("visible");
+      document.getElementById("indigency").classList.toggle("visible");
+
+      document.querySelector(".closeCertificate").addEventListener("click", function () {
+        console.log("close click");
+        document.getElementById("certificate").classList.remove("visible");
+        document.getElementById("indigency").classList.remove("visible");
+      })
+    })
+  }
+  else if (purpose === 'Land no claim') {
+    viewCertificate.addEventListener('click', function () {
+      document.getElementById("certificate").classList.toggle("visible");
+      document.getElementById("landNoClaim").classList.toggle("visible");
+
+      document.querySelector(".closeCertificate").addEventListener("click", function () {
+        console.log("close click");
+        document.getElementById("certificate").classList.remove("visible");
+        document.getElementById("landNoClaim").classList.remove("visible");
+      })
+    })
+  }
+  else if (purpose === 'Late Registration') {
+  }
+  else if (purpose === 'No Duplication') {
+    viewCertificate.addEventListener('click', function () {
+      document.getElementById("certificate").classList.toggle("visible");
+      document.getElementById("noDuplication").classList.toggle("visible");
+
+      document.querySelector(".closeCertificate").addEventListener("click", function () {
+        console.log("close click");
+        document.getElementById("certificate").classList.remove("visible");
+        document.getElementById("noDuplication").classList.remove("visible");
+      })
+    })
+  }
+  else if (purpose === 'Panumduman') {
+    viewCertificate.addEventListener('click', function () {
+      document.getElementById("certificate").classList.toggle("visible");
+      document.getElementById("panumduman").classList.toggle("visible");
+
+      document.querySelector(".closeCertificate").addEventListener("click", function () {
+        console.log("close click");
+        document.getElementById("certificate").classList.remove("visible");
+        document.getElementById("panumduman").classList.remove("visible");
+      })
+    })
+  }
+  else if (purpose === 'RA 11261') {
+    viewCertificate.addEventListener('click', function () {
+      document.getElementById("certificate").classList.toggle("visible");
+      document.getElementById("RA11261").classList.toggle("visible");
+
+      document.querySelector(".closeCertificate").addEventListener("click", function () {
+        console.log("close click");
+        document.getElementById("certificate").classList.remove("visible");
+        document.getElementById("RA11261").classList.remove("visible");
+      })
+    })
+  }
+  else if (purpose === 'Residency') {
+    alert(purpose);
+  }
+  else if (purpose === 'Same Person') {
+    alert(purpose);
+  }
+  else if (purpose === 'Singleness') {
+    alert(purpose);
+  }
+  else if (purpose === 'Solo Parent') {
+    alert(purpose);
+  }
+};
