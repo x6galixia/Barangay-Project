8f401aac4a2de0862120e25ac09c04646f0eb8d3
x6galixia <x6galixia@gmail.com>
2024-12-17 03:37:12 +0800

    statistics done

--- middlewares/helper-functions/calculations.js
+++ middlewares/helper-functions/calculations.js
@@ -1,25 +1,38 @@
 function calculateAge(birthDate) {
-    if (!birthDate) {
-      return null;
-    }
-  
-    const birthDateObj = new Date(birthDate);
-  
- 
-    if (isNaN(birthDateObj)) {
-      return null;
-    }
-  
-    const today = new Date();
-    let age = today.getFullYear() - birthDateObj.getFullYear();
-    const monthDifference = today.getMonth() - birthDateObj.getMonth();
-  
-    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDateObj.getDate())) {
-      age--;
-    }
-  
-    return age;
+  if (!birthDate) {
+    return null;
   }
-    
 
-module.exports = calculateAge;
+  const birthDateObj = new Date(birthDate);
+
+  if (isNaN(birthDateObj)) {
+    return null;
+  }
+
+  const today = new Date();
+  let age = today.getFullYear() - birthDateObj.getFullYear();
+  const monthDifference = today.getMonth() - birthDateObj.getMonth();
+
+  if (
+    monthDifference < 0 ||
+    (monthDifference === 0 && today.getDate() < birthDateObj.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+function getCurrentDate() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
+
+module.exports = {
+  calculateAge,
+  getCurrentDate,
+};
