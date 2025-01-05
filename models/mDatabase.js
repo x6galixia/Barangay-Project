c2a9c4d87baacefa83a7cdf26013a91faf689a36
x6galixia <x6galixia@gmail.com>
2025-01-05 17:24:12 +0800

    ready for turnover

--- models/mDatabase.js
+++ models/mDatabase.js
@@ -1,11 +1,24 @@
-const Pool = require('pg').Pool
-require('dotenv').config()
+const { Pool } = require('pg');
+require('dotenv').config();
 
-const mPool = new Pool({
+const poolConfig = {
     connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
-  });
+};
 
-module.exports = mPool
+console.log('Database configuration:', {
+    connectionString: poolConfig.connectionString,
+    ssl: poolConfig.ssl,
+});
+
+const mPool = new Pool(poolConfig);
+
+mPool.on('error', (err) => {
+    console.error('Unexpected error on idle database client', err);
+    process.exit(-1);
+});
+
+module.exports = mPool;
